b503f1f326b04dd12f6bccb743b79e41263944cf
Tuan Pham <phamtuancules20@gmail.com>
2023-09-05 17:56:49 +0700

    progress vote

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,34 @@
-import { Button, StyleSheet, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-const ButtonCustom = ({ title, color }) => {
+const ButtonCustom = ({ title, color, style }) => {
+  console.log('style', style);
   return (
-    <View>
-      <Button title='+ Tạo Quỹ/thu/chi' style={styles.button} />
-    </View>
+    <TouchableOpacity style={[styles.appButtonContainer, { ...style }]}>
+      <Text style={styles.appButtonText}>+ Tạo Quỹ/Thu/Chi</Text>
+    </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-  button: {
-    backgroundColor: 'transparent',
+  appButtonContainer: {
+    elevation: 8,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    borderWidth: 1,
+    borderColor: '#F58F00',
+    width: 180,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    // flexDirection: 'row',
+  },
+  appButtonText: {
+    fontSize: 14,
     color: '#F58F00',
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    // textTransform: 'uppercase',
   },
 });
 
